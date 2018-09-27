f54082ccc67459cce1b8dfd549e97cb2a3a2b974
Tsanko Georgiev <georgtsa@tools.adidas-group.com>
2018-09-27 15:35:45 +0300

    Created POJO for Node abstract class

--- src/main/java/com/graph/model/Node.java
+++ src/main/java/com/graph/model/Node.java
@@ -1,5 +1,38 @@
 package com.graph.model;
 
 public abstract class Node {
+    private Node leftChildNode;
+    private Node rightChildNode;
+    private int result;
+
+    public Node(Node leftChildNode, Node rightChildNode, int result) {
+        this.leftChildNode = leftChildNode;
+        this.rightChildNode = rightChildNode;
+        this.result = result;
+    }
+
+    public Node getLeftChildNode() {
+        return leftChildNode;
+    }
+
+    public void setLeftChildNode(Node leftChildNode) {
+        this.leftChildNode = leftChildNode;
+    }
+
+    public Node getRightChildNode() {
+        return rightChildNode;
+    }
+
+    public void setRightChildNode(Node rightChildNode) {
+        this.rightChildNode = rightChildNode;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
 
 }
