ae5eff1b5adcdf8a1205e54bcb1a6334569e0b99
Georgi Vladimirov <Georgi_Vladimirov@epam.com>
2018-10-03 09:50:14 +0300

    GraphPrinter done

--- src/main/java/com/graph/Application.java
+++ src/main/java/com/graph/Application.java
@@ -1,18 +1,21 @@
 package com.graph;
 
-import com.graph.exception.InvalidInputException;
 import com.graph.facade.GraphPrinter;
 import com.graph.model.Graph;
+import com.graph.model.Node;
+import com.graph.model.NumberNode;
 
 public class Application {
 
-    public static void main(String[] args) throws InvalidInputException {
+    public static void main(String[] args){
 
         Graph graph = new Graph();
         GraphPrinter graphPrinter = new GraphPrinter();
+        Node node = new NumberNode(1235);
+        Node node1 = new NumberNode(23.55);
+        graph.getGraph().get(0).get(0).initializeSquare("", node);
+        graph.getGraph().get(9).get(27).initializeSquare("", node1);
         graphPrinter.printGraph(graph);
-
-
 
     }
 
--- src/main/java/com/graph/model/Graph.java
+++ src/main/java/com/graph/model/Graph.java
@@ -1,37 +1,20 @@
 package com.graph.model;
 
 import java.util.ArrayList;
+import static com.graph.converter.Converter.rowColumnToName;
 
 public class Graph {
 
     private ArrayList<ArrayList<Square>> graph;
 
     public Graph(){
-        graph = new ArrayList<ArrayList<Square>>();
+        graph = new ArrayList<>();
         for (int i = 1; i<999; i++){
-            graph.add(new ArrayList<Square>());
+            graph.add(new ArrayList<>());
             for (int j = 0; j<=702; j++){
-                graph.get(i-1).add(new Square(this.rowColumnToName(i, j), Square.Status.NOT_INITIALIZED));
+                graph.get(i-1).add(new Square(rowColumnToName(i, j), Square.Status.NOT_INITIALIZED));
             }
         }
-    }
-
-    private String rowColumnToName(int row, int column){
-        String coordinates = "";
-        int firstLetterNumber = column / 26;
-        if (firstLetterNumber > 0){
-            int firstLetterCode = firstLetterNumber + 64;
-            char firstLetter = (char) firstLetterCode;
-            coordinates = coordinates.concat(Character.toString(firstLetter));
-        }
-        int secondLetterNumber = column % 26;
-        int secondLetterCode = secondLetterNumber + 65;
-        char secondLetter = (char) secondLetterCode;
-
-        coordinates = coordinates.concat(Character.toString(secondLetter));
-        coordinates = coordinates.concat(Integer.toString(row));
-
-        return coordinates;
     }
 
     public ArrayList<ArrayList<Square>> getGraph() {
