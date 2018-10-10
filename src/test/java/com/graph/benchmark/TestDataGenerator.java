7fe392026a72b9c227149148aebbcac793dff011
Tsanko Georgiev <georgtsa@tools.adidas-group.com>
2018-10-10 16:11:32 +0300

    created test data generator

--- src/test/java/com/graph/benchmark/TestDataGenerator.java
+++ src/test/java/com/graph/benchmark/TestDataGenerator.java
@@ -1,11 +1,13 @@
 package com.graph.benchmark;
 
+import com.graph.converter.Converter;
 import com.graph.exception.CellNotInitializedException;
 import com.graph.exception.CircularDependenciesException;
 import com.graph.exception.InvalidInputException;
 import com.graph.exception.ParseException;
 import com.graph.facade.GraphFacade;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -17,16 +19,37 @@ public class TestDataGenerator {
         throw new IllegalStateException();
     }
 
-    public static void generateData(GraphFacade graphFacade, int sizeOfData) {
-        //TODO
-        //generate expression string
-        //generate dependencies for current cell;
-        //process expression from graphFacade
+    public static String generateData(GraphFacade graphFacade, int sizeOfData) {
+        String lastDependencyName = Converter.columnToLetters(sizeOfData - 1) + "1";
         try {
-            graphFacade.processExpression("A1=B1+C1");
-            graphFacade.processExpression("B1=C1");
+            StringBuilder expression = new StringBuilder();
+            expression.append("A1=");
+            for (int i = 0; i < sizeOfData - 2; i++) {
+                String reference = Converter.columnToLetters(i + 1);
+                expression.append(reference).append(1);
+                StringBuilder dependencies = new StringBuilder();
+                dependencies.append(reference).append(1).append("=");
+                System.out.println(expression);
+                for (int j = i + 1; j < sizeOfData - 1; j++) {
+                    String dependencyReference = Converter.columnToLetters(j + 1);
+                    dependencies.append(dependencyReference).append(1).append(generateRandomArithmeticOperation());
+                }
+                dependencies.append(1);
+                graphFacade.processExpression(dependencies.toString());
+                expression.append(generateRandomArithmeticOperation());
+                System.out.println(dependencies);
+            }
+            expression.append(1);
+            graphFacade.processExpression(expression.toString());
         } catch (InvalidInputException | CircularDependenciesException | CellNotInitializedException | ParseException | InterruptedException | ExecutionException e) {
+            System.out.println("ERROR");
             e.getMessage();
         }
+        return lastDependencyName;
+    }
+
+    private static String generateRandomArithmeticOperation() {
+        String[] symbols = {"-", "+", "/", "*", "^"};
+        return symbols[new Random().nextInt(symbols.length)];
     }
 }
